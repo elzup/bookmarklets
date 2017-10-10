eaebb6774efda8876067c35c8c5790171a8abd61
elzup <guild0105@gmail.com>
2017-10-10 11:59:02 +0900

    Now Browsing

---
+++ tweet-nowbrowsing.js
@@ -0,0 +1,8 @@
+// Twitter 共有 Window を開く
+javascript: (() => {
+	const url = window.location.href;
+	const encodedUrl = encodeURIComponent(url);
+	const text = encodeURIComponent(`見てる - ${document.title}`);
+	const twitterShareUrl = `https://twitter.com/intent/tweet?text=${text}&url=${encodedUrl}&hashtags=NowBrowsing`;
+	window.open(twitterShareUrl, '', 'width=550, height=420');
+})();
