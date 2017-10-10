b1f820981dd35af4ffcb3db6aa0154ad3d6424f0
elzup <guild0105@gmail.com>
2017-10-10 12:19:35 +0900

    Compep dumper

---
+++ compep-atcoder-samplecase-parse.js
@@ -0,0 +1,20 @@
+// AtCoder の問題ページから compep のテストケースを生成する
+//  userscript の方がいいかもしれない
+javascript: (() => {
+	const codes = [].map.call(
+		document.querySelectorAll('.div-sample-copy'),
+		e => e.nextSibling.innerHTML
+	);
+	const n = 2;
+	const len = Math.round(codes.length / n, 10);
+	const ret = [];
+	for (var i = 0; i < len / 2; i++) {
+		ret.push(codes.slice(i * n, i * n + n));
+	}
+	const text = ret
+		.map(e => {
+			return e.join('----\n');
+		})
+		.join('====\n');
+	document.write('<pre>' + text + '</pre>');
+})();
