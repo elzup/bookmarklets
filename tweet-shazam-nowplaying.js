094684c208d2d5377d9e1df05a4ffebb37a95dd0
elzup <guild0105@gmail.com>
2018-03-11 11:35:52 +0900

    Shazam Nowplay

---
+++ tweet-shazam-nowplaying.js
@@ -0,0 +1,13 @@
+// Shazam から Twitter 共有 Window を開く
+javascript: (() => {
+  const songTitle = document.title.replace(' | Shazam', '');
+  const url = encodeURIComponent(window.location.href);
+  const text = encodeURIComponent(`♫['w'] < ${songTitle}`);
+  const hashtags = 'NowPlaying';
+  const params = { url, text, hashtags };
+  const query = Object.entries(params)
+    .map((kv) => kv.join('='))
+    .join('&');
+  const shareUrl = `https://twitter.com/intent/tweet?` + query;
+  window.open(shareUrl, '', 'width=550, height=420');
+})();
