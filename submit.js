223f88c23f355539753b00003d50c652956e0bf4
elzup <guild0105@gmail.com>
2021-07-26 09:48:26 +0900

    add submit sample

---
+++ submit.js
@@ -0,0 +1,5 @@
+javascript: () => {
+  document.getElementsByClassName('open-form-button')[0].click();
+  document.getElementById('text-input').value = 'message here';
+  document.getElementById('submit-button').click();
+};
